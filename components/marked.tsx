49288789084a38413f01a1769e0142a7f8e68ffd
charlesacosta163 <charlesacosta163@gmail.com>
2024-07-18 22:37:38 -0400

    removed react syntax highlighter

--- components/marked.tsx
+++ components/marked.tsx
@@ -2,8 +2,6 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import remarkBreaks from 'remark-breaks';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Choose your preferred theme
 
 const MarkedComponent = ({ source }: { source: string }) => {
   return (
@@ -12,25 +10,6 @@ const MarkedComponent = ({ source }: { source: string }) => {
       remarkPlugins={[remarkGfm, remarkBreaks]}
       rehypePlugins={[rehypeRaw]}
       className="markdown leading-8"
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={dracula}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
     />
   );
 };
