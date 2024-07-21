96adb599ee259f88db869086f1e00bfc9c515725
charlesacosta163 <charlesacosta163@gmail.com>
2024-07-21 00:05:29 -0400

    Added twitter meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,43 @@ import Head from "next/head";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://charlesac.dev"),
   title: "Charles Acosta",
-  description: "Portfolio made from Next 14",
+  description: "Hi, I'm Charles Acosta! Check out my web development projects and blog posts, and learn about my journey in tech.",
+  keywords: ["Next.js", "TypeScript", "Web Development", "College", "University", "Programming"],
+  referrer: 'origin-when-cross-origin',
+  creator: "Charles Acosta",
+  openGraph: {
+    type: 'website',
+    title: "Charles Acosta",
+    description: "Hi, I'm Charles Acosta! Check out my web development projects and blog posts, and learn about my journey in tech.",
+    url: "https://charlesac.dev",
+    siteName: "Charles Acosta",
+    images: [{
+      url: "https://images.unsplash.com/photo-1508830524289-0adcbe822b40?q=80&w=2025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      width: 800,
+      height: 600
+    }, {
+      url: "https://images.unsplash.com/photo-1472437774355-71ab6752b434?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      width: 800,
+      height: 600
+    }]
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@charlesacosta163",
+    title: "Charles Acosta",
+    description: "Hi, I'm Charles Acosta! Check out my web development projects and blog posts, and learn about my journey in tech.",
+    images: [{
+      url: "https://images.unsplash.com/photo-1508830524289-0adcbe822b40?q=80&w=2025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      width: 800,
+      height: 600
+    }, {
+      url: "https://images.unsplash.com/photo-1472437774355-71ab6752b434?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      width: 800,
+      height: 600
+    }]
+  }
 };
 
 export default function RootLayout({
