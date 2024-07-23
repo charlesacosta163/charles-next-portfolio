32e4e15442ff581676feff62e64dc057baf53cf1
charlesacosta163 <charlesacosta163@gmail.com>
2024-07-22 23:20:32 -0400

    modified analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,15 +84,35 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <link rel="icon" href="../app/favicon.ico" />
+
+        <Script
+          id="gtag-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${process.env.GTM_ID}');
+          `,
+          }}
+        />
       </Head>
 
       <body
         className={`bg-body-light text-text-light dark:bg-body-dark dark:text-text-dark poppins-regular `}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
-
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_G_ID as string} />
     </html>
   );
 }
